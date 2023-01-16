d414966949fb6c8cfd5f22b9ac1fc84570104606
Kobe Wulteputte <kobe.wulteputte@orbid.be>
2023-01-16 14:51:11 +0100

    Function setup

---
+++ BS.Function/ExpenseCategoryLearner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+using BS.Logic;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace BS.Function;
+
+public class ExpenseCategoryLearner
+{
+    private readonly ILogger<ExpenseCategoryLearner> _logger;
+    private readonly InstitutionService _institutionService;
+
+    public ExpenseCategoryLearner(ILogger<ExpenseCategoryLearner> logger,
+        InstitutionService institutionService)
+    {
+        _logger = logger;
+        _institutionService = institutionService;
+    }
+
+    [Function("LearnExpenses")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequestData req)
+    {
+        var sw = new Stopwatch();
+        sw.Restart();
+
+        var institution = await _institutionService.GetUsedInstitutions();
+        _logger.LogInformation("institution: {institution}", institution);
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+
+        response.Headers.Add("Date", "Mon, 18 Jul 2016 16:06:00 GMT");
+        response.Headers.Add("Content-Type", "text/html; charset=utf-8");
+        response.WriteString(JsonSerializer.Serialize(institution));
+
+        // _logger.LogMetric(@"funcExecutionTimeMs", sw.Elapsed.TotalMilliseconds,
+        //     new Dictionary<string, object>
+        //     {
+        //         { "foo", "bar" },
+        //         { "baz", 42 }
+        //     }
+        // );
+
+        return response;
+    }
+}
--- BS.Function/Program.cs
+++ BS.Function/Program.cs
@@ -1,13 +1,22 @@
+using System.Net.Mime;
+using System.Reflection;
+using BS.Logic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NodaTime;
 using VMelnalksnis.NordigenDotNet.DependencyInjection;
-using BS.Console;
-using BS.Logic;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureHostConfiguration(cfg => { cfg.AddJsonFile("appsettings.json"); })
+var host = new HostBuilder()
+    .ConfigureAppConfiguration((hostContext, builder) =>
+    {
+        builder
+            .AddJsonFile("local.settings.json", true)
+            .AddUserSecrets(Assembly.GetExecutingAssembly(), false);
+    })
+    .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices((ctx, services) =>
     {
-        services.AddSingleton<Application, Application>();
         services.AddSingleton<IClock>(SystemClock.Instance);
         services.AddSingleton(DateTimeZoneProviders.Tzdb);
         services.AddTransient<InstitutionService, InstitutionService>();
@@ -17,15 +26,7 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddTransient<WorkbookService, WorkbookService>();
         services.AddTransient<ExpenseService, ExpenseService>();
         services.AddNordigenDotNet(ctx.Configuration);
-    }).ConfigureLogging((context, cfg) =>
-    {
-        cfg.ClearProviders();
-        cfg.AddConfiguration(context.Configuration.GetSection("Logging"));
-        cfg.AddConsole();
     })
     .Build();
 
-var app = host.Services.GetRequiredService<Application>();
-await app.Run();
-Console.WriteLine("Done!");
-Console.ReadLine();
+host.Run();
